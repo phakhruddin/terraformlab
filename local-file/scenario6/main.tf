658c15719527b089c70bfba483297f54ab4bf17e
Deen Abdullah <phakhruddin@gmail.com>
2024-10-15 14:32:56 -0700

    feat(T-0001): upd scenario6

--- local-file/scenario6/main.tf
+++ local-file/scenario6/main.tf
@@ -29,3 +29,31 @@ resource "null_resource" "step_log" {
 output "instance_details" {
   value = { for key, value in var.instances : key => "Instance type is ${value}" }
 }
+
+#external yaml file example
+
+provider "local" {}
+
+# Read the YAML file containing the AMI configurations
+data "local_file" "ami_config" {
+  filename = "${path.module}/input.yaml"
+}
+
+# Decode the YAML file content
+locals {
+  ami_data = yamldecode(data.local_file.ami_config.content).amis
+}
+
+# Iterate over the AMI numbers and architectures from the YAML file
+resource "null_resource" "print_ami_info" {
+  for_each = { for ami_info in local.ami_data : ami_info.ami => ami_info }
+
+  provisioner "local-exec" {
+    command = "echo 'AMI: ${each.value.ami}, Architecture: ${each.value.architecture}'"
+  }
+}
+
+# Output the AMI and architecture details after execution
+output "ami_architecture_details" {
+  value = { for ami_info in local.ami_data : ami_info.ami => "Architecture: ${ami_info.architecture}" }
+}
