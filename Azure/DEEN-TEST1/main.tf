07bf393e0d843659846040c7554724d28701a37f
Deen Abdullah <phakhruddin@gmail.com>
2025-03-09 13:12:08 -0700

    feat: add subnets

--- Azure/DEEN-TEST1/main.tf
+++ Azure/DEEN-TEST1/main.tf
@@ -21,4 +21,73 @@ resource "azurerm_route_table" "my_route_table" {
   name                = "myRouteTable"
   resource_group_name = azurerm_resource_group.deen_test1.name
   location            = azurerm_resource_group.deen_test1.location
+
+  route {
+    name                   = "AllowSubnetCommunication"
+    address_prefix         = "10.0.0.0/16"
+    next_hop_type          = "VnetLocal"
+  }
+}
+
+
+resource "azurerm_virtual_network" "myVNet" {
+  name                = "myVNet"
+  location            = "westus2"
+  resource_group_name = "deen_test1"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "ingest" {
+  name                 = "ingest-subnet"
+  resource_group_name  = azurerm_virtual_network.myVNet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "data" {
+  name                 = "data-subnet"
+  resource_group_name  = azurerm_virtual_network.myVNet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_virtual_network.myVNet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
+
+resource "azurerm_subnet" "general" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_virtual_network.myVNet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_virtual_network.myVNet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.5.0/26"]
+}
+
+resource "azurerm_subnet_route_table_association" "ingest" {
+  subnet_id      = azurerm_subnet.ingest.id
+  route_table_id = azurerm_route_table.my_route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "data" {
+  subnet_id      = azurerm_subnet.data.id
+  route_table_id = azurerm_route_table.my_route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "private" {
+  subnet_id      = azurerm_subnet.private.id
+  route_table_id = azurerm_route_table.my_route_table.id
+}
+
+resource "azurerm_subnet_route_table_association" "general" {
+  subnet_id      = azurerm_subnet.general.id
+  route_table_id = azurerm_route_table.my_route_table.id
 }
